1245e0e8b6a920f5dafdddad6f8e870616988e27
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-17 00:35:50 +0700

    Create ytdl.py

---
+++ tabs/extra/ytdl/ytdl.py
@@ -0,0 +1,58 @@
+import gradio as gr
+import yt_dlp
+
+def download_youtube_audio(youtube_url, custom_file_name):
+    # Define yt-dlp options
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+        }],
+        'outtmpl': f'{cfile}.wav',
+    }
+    
+    # Download the audio
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url])
+    
+    return f'Downloaded and saved as {cfile}.wav  Code by Blane'
+
+
+def ytdl():
+    with gr.Accordion(label=("download youtube acappela"):
+        with gr.Column():
+            url = gr.Textbox(
+                label=("url to audio"),
+                value="",
+                max_lines=1,
+                interactive=True,
+                placeholder=("https://youtu.be/iN0-dRNsmRM?si=42PgawH73GIrvYLs"),
+            )
+            cfile = gr.Textbox(
+                label=("custom file name"),
+                value="",
+                interactive=True,
+                placeholder=("cfile"),
+            )
+
+        outputs = gr.Textbox(
+            label=("Output Information"),
+            value="",
+        )
+
+        ytdl_button = gr.Button(
+            ("Fusion"), variant="primary", interactive=False
+        )
+
+        ytdl_button.click(
+            ytdl,
+            [
+                url,
+                cfile,
+            ],
+            outputs,
+            api_name="ytdl",
+        )
+
